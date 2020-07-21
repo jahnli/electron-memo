251616da6a3ee3202c4076a87bb21457ee4779d5
Jahn <jahnli@163.com>
2020-07-21 14:22:12 +0800

    T

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import  "./plugins/ant";
+const electron = window.require('electron');
+Vue.prototype.$electron = electron;
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
