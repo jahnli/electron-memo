5e2a396a8724031136f87c9e9fb6d81c68d66e65
Jahn <jahnli@163.com>
2020-07-24 08:57:59 +0800

    T

--- src/main-process/tray.js
+++ src/main-process/tray.js
@@ -4,34 +4,9 @@ let tray;
 app.whenReady().then(() => {
   const win  = BrowserWindow.getFocusedWindow();
   tray = new Tray(path.join(__static,'icon.png'));
-  const menus = [
-    {label: '打开主界面', click:show},
-    {type:'separator',visible:false},
-    {label: '锁定',visible:false},
-    {label: '设置',visible:false},
-    {type:'separator',visible:false},
-    {label: '最小化'},
-    {
-      label: '退出登录',
-      click:()=>{
-        win.webContents.send('home');
-      },
-      visible:false
-    },
-    {
-      label: '退出程序',
-      click: () => {
-        win.destroy();
-        app.quit();
-      }
-    }
-  ];
-  let contextMenu = Menu.buildFromTemplate(menus)
-
-  tray.setToolTip('便签')
-  tray.setContextMenu(contextMenu);
   // 监听追加菜单
   addTrayMenu();
+  tray.setToolTip('便签')
   tray.on('click', () => {
     win.show()
   })
@@ -41,19 +16,48 @@ app.whenReady().then(() => {
   }
   function addTrayMenu() {
     ipcMain.on('changeTray',function (event, args) {
-      console.log(contextMenu.items[1].type);
       if(args){
-        contextMenu.items[1].type = 'separator';
+        const completeMenus = [
+          {label: '打开主界面', click:show},
+          {type:'separator'},
+          {label: '锁定'},
+          {label: '设置'},
+          {type:'separator'},
+          {label: '最小化'},
+          {
+            label: '退出登录',
+            click:()=>{
+              win.webContents.send('home');
+            },
+            visible:false
+          },
+          {
+            label: '退出程序',
+            click: () => {
+              win.destroy();
+              app.quit();
+            }
+          }
+        ];
+        let contextMenu = Menu.buildFromTemplate(completeMenus)
+        tray.setContextMenu(contextMenu);
       }else{
-        contextMenu.items[1].type = 'normal';
-        contextMenu.items[1].label = '';
-        contextMenu.items[1].visible = false;
+        const baseMenu = [
+          {label: '打开主界面', click:show},
+          {type:'separator'},
+          {label: '最小化'},
+          {
+            label: '退出程序',
+            click: () => {
+              win.destroy();
+              app.quit();
+            }
+          }
+        ]
+        let contextMenu = Menu.buildFromTemplate(baseMenu)
+        tray.setContextMenu(contextMenu);
       }
-      console.log(contextMenu.items[1].type);
-      contextMenu.items[2].visible = args;
-      contextMenu.items[3].visible = args;
-      contextMenu.items[6].visible = args;
-      tray.setContextMenu(contextMenu)
+
     })
   }
 })
